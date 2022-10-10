98d31632f4e31cefc803679ca8f1c38dfc6b5c1e
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2022-10-10 21:47:37 +0000

    Added numGuess.cpp

---
+++ numGuess.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2022 Peter Sobowale All rights reserved.
+//
+// This program asks the user for a random number from
+// 0 to 9 and checks if it is the correct number
+#include <iostream>
+
+
+int main() {
+    // declare constants
+    const int CORRECT_NUMBER = 9;
+
+    // declare variables
+    int randomNumber;
+
+    // get random number
+    std::cout << "Enter a number from 0 to 9: ";
+    std::cin >> randomNumber;
+
+    // if randomNumber == CORRECT_NUMBER
+    if (randomNumber == CORRECT_NUMBER) {
+        std::cout << "You guessed correctly!";
+    }
+
+    // if randomNumber <> CORRECT_NUMBER
+    if (randomNumber != CORRECT_NUMBER) {
+        std::cout << "You guessed wrong :(";
+    }
+}
